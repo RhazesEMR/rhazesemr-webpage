9aaaec3e9d86db37a1476ecf3433caff31e4aeac
Naz <ndpm13@ch-naseem.com>
2025-03-06 20:03:38 +0100

    ✨feat: add a link to docs

--- src/lib/share/Header.svelte
+++ src/lib/share/Header.svelte
@@ -7,16 +7,21 @@
   import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
-<header
-  class="fixed top-0 flex justify-between p-3 w-full bg-background border-b"
->
-  <button onclick={() => goto("/#")}>
-    <h1 class="text-xl">
-      <i class="fa-solid fa-flask"></i> RhazesEMR
-    </h1>
-  </button>
-
-  <div>
+<header class="fixed top-0 flex justify-between items-center p-3 w-full bg-background border-b">
+  <div class="flex items-center gap-4">
+    <button on:click={() => goto("/#")}>
+      <h1 class="text-xl">
+        <i class="fa-solid fa-flask"></i> RhazesEMR
+      </h1>
+    </button>
+    <button on:click={() => window.location.href = "https://docs.rhazesemr.info"}>
+      <h1 class="text-lg">
+        Docs
+      </h1>
+    </button>
+  </div>
+  
+  <div class="flex items-center gap-2">
     <a href="https://github.com/RhazesEMR">
       <Button variant="outline" size="icon">
         <Github />
