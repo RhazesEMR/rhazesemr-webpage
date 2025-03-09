07be694991ee4108f6b6b5d8fbd9085e432607f6
Naz <ndpm13@ch-naseem.com>
2025-03-09 18:24:33 +0100

    ✨feat: create a new route for about

--- src/lib/share/Header.svelte
+++ src/lib/share/Header.svelte
@@ -7,30 +7,31 @@
   import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
-<header class="fixed top-0 flex justify-between items-center p-3 w-full bg-background border-b mb-32">
+<header
+  class="fixed top-0 flex justify-between items-center p-3 w-full bg-background border-b mb-32"
+>
   <div class="flex items-center gap-4">
     <button on:click={() => goto("/#")}>
       <h1 class="text-xl font-bold">
         <i class="fa-solid fa-flask"></i> RhazesEMR
       </h1>
     </button>
-    <button on:click={() => window.location.href = "https://docs.rhazesemr.info"}>
-      <h1 class="text-lg">
-        Docs
-      </h1>
+    <button
+      on:click={() => (window.location.href = "https://docs.rhazesemr.info")}
+    >
+      <h1 class="text-lg">Docs</h1>
     </button>
     <button on:click={() => goto("/download")}>
-      <h1 class="text-lg">
-        Download
-      </h1>
+      <h1 class="text-lg">Download</h1>
     </button>
     <button on:click={() => goto("/features")}>
-      <h1 class="text-lg">
-        Features
-      </h1>
+      <h1 class="text-lg">Features</h1>
+    </button>
+    <button on:click={() => goto("/about")}>
+      <h1 class="text-lg">About</h1>
     </button>
   </div>
-  
+
   <div class="flex items-center gap-2">
     <a href="https://github.com/RhazesEMR">
       <Button variant="outline" size="icon">
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Info } from "lucide-svelte";
+</script>
+
+<main class="flex flex-grow flex-col items-center mt-28">
+  <div class="m-3 text-4xl flex items-center animate-bounce">
+    <Info size={52} />
+    <p class="px-2">About</p>
+    <Info size={52} />
+  </div>
+
+  <div>
+    <p class="text-lg max-w-3xl text-center p-5">
+      RhazesEMR began as a graduation project with the mission to create an
+      accessible, open-source electronic medical records system. Named after the
+      pioneering Muslim physician Abu Bakr al-Razi, this platform aims to
+      provide healthcare professionals with modern tools that respect data
+      privacy and security while remaining FOSS and customizable.
+    </p>
+  </div>
+</main>
