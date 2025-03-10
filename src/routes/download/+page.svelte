4e2cf980587e0911ed60111a6fb6dca6fe4484cf
Naz <ndpm13@ch-naseem.com>
2025-03-10 20:27:50 +0100

    ✨feat: disabling download buttons for now till we have releases

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -36,7 +36,7 @@
     apk: "",
   };
 
-  let github_release = "";
+  let github_release = "https://github.com/RhazesEMR/rhazesemr-client/releases";
 </script>
 
 <main class="flex flex-grow flex-col items-center mt-28">
@@ -56,17 +56,17 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pb-4">
-          <i class="fa-brands fa-windows text-9xl"></i>
+          <i class="animate-pulse fa-brands fa-windows text-9xl"></i>
         </div>
-
-        <div class="flex flex-grow justify-center">
-          <Button class="w-1/2 mr-1" href={win_release.exe}>
-            <Download /> EXE
-          </Button>
-          <Button class="w-1/2" href={win_release.msi}>
-            <Download /> MSI
-          </Button>
-        </div>
+        <!-- <div class="flex flex-grow justify-center"> <Button class="w-1/2 mr-1" href={win_release.exe}> <Download /> EXE </Button> <Button class="w-1/2" href={win_release.msi}> <Download /> MSI </Button> </div> -->
+        <button
+          onclick={() =>
+            toast.warning("Not available yet", {
+              description: "this release is still in need for further testing",
+            })}
+        >
+          <Button class="w-full" disabled>Coming soon...</Button>
+        </button>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
@@ -82,11 +82,17 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pt-4">
-          <i class="fa-brands fa-android text-9xl"></i>
+          <i class="animate-pulse fa-brands fa-android text-9xl"></i>
         </div>
-        <Button class="w-full" href={android_release.apk}>
-          <Download /> APK
-        </Button>
+        <!-- <Button class="w-full" href={android_release.apk}> <Download /> APK </Button> -->
+        <button
+          onclick={() =>
+            toast.warning("Not available yet", {
+              description: "this release is still in need for further testing",
+            })}
+        >
+          <Button class="w-full" disabled>Coming soon...</Button>
+        </button>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
@@ -102,19 +108,17 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pt-2 pb-2">
-          <i class="fa-brands fa-linux text-9xl"></i>
+          <i class="animate-pulse fa-brands fa-linux text-9xl"></i>
         </div>
-        <div class="flex flex-grow justify-center">
-          <Button class="w-1/3 mr-1" href={linux_release.deb}>
-            <Download /> DEB
-          </Button>
-          <Button class="w-1/3 mr-1" href={linux_release.rpm}>
-            <Download /> RPM
-          </Button>
-          <Button class="w-1/3" href={linux_release.appimage}>
-            <Download /> AppImage
-          </Button>
-        </div>
+        <!-- <div class="flex flex-grow justify-center"> <Button class="w-1/3 mr-1" href={linux_release.deb}> <Download /> DEB </Button> <Button class="w-1/3 mr-1" href={linux_release.rpm}> <Download /> RPM </Button> <Button class="w-1/3" href={linux_release.appimage}> <Download /> AppImage </Button> </div> -->
+        <button
+          onclick={() =>
+            toast.warning("Not available yet", {
+              description: "this release is still in need for further testing",
+            })}
+        >
+          <Button class="w-full" disabled>Coming soon...</Button>
+        </button>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
