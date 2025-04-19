a76eb90d15ae5e8cba38d72a7cdae908a2511eed
Naz <ndpm13@ch-naseem.com>
2025-04-19 15:33:40 +0100

    ✨feat: test release

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -8,21 +8,23 @@
   let win_release = {
     title: "Windows Installer",
     description: "RhazesEMR Desktop Client for Windows",
-    msi: "",
-    exe: "",
+    msi: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client_0.0.1_x64_en-US.msi/download",
+    exe: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client_0.0.1_x64-setup.exe/download",
   };
 
   let mac_release = {
     title: "MacOS Installer",
     description: "RhazesEMR Desktop Client for MacOS",
+    dmg: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client_0.0.1_aarch64.dmg/download",
   };
 
   let linux_release = {
     title: "Linux Installer",
     description: "RhazesEMR Desktop Client for Linux",
-    deb: "",
-    rpm: "",
-    appimage: "",
+    deb: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client_0.0.1_amd64.deb/download",
+    rpm: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client-0.0.1-1.x86_64.rpm/download",
+    appimage:
+      "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client_0.0.1_amd64.AppImage/download",
   };
 
   let ios_release = {
@@ -33,7 +35,7 @@
   let android_release = {
     title: "Android Installer",
     description: "RhazesEMR Mobile Client for Android",
-    apk: "",
+    apk: "https://sourceforge.net/projects/rhazesemr/files/v0.0.1-dev/rhazesemr-client-0.0.1-universal.apk/download",
   };
 
   let github_release = "https://github.com/RhazesEMR/rhazesemr-client/releases";
@@ -56,17 +58,14 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pb-4">
-          <i class="animate-pulse fa-brands fa-windows text-9xl"></i>
+          <i class="fa-brands fa-windows text-9xl"></i>
         </div>
-        <!-- <div class="flex flex-grow justify-center"> <Button class="w-1/2 mr-1" href={win_release.exe}> <Download /> EXE </Button> <Button class="w-1/2" href={win_release.msi}> <Download /> MSI </Button> </div> -->
-        <button
-          onclick={() =>
-            toast.warning("Not available yet", {
-              description: "this release is still in need for further testing",
-            })}
-        >
-          <Button class="w-full" disabled>Coming soon...</Button>
-        </button>
+        <div class="flex flex-grow justify-center">
+          <Button class="w-1/2 mr-1" href={win_release.exe}>
+            <Download /> EXE
+          </Button>
+          <Button class="w-1/2" href={win_release.msi}><Download /> MSI</Button>
+        </div>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
@@ -82,17 +81,11 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pt-4">
-          <i class="animate-pulse fa-brands fa-android text-9xl"></i>
+          <i class="fa-brands fa-android text-9xl"></i>
         </div>
-        <!-- <Button class="w-full" href={android_release.apk}> <Download /> APK </Button> -->
-        <button
-          onclick={() =>
-            toast.warning("Not available yet", {
-              description: "this release is still in need for further testing",
-            })}
-        >
-          <Button class="w-full" disabled>Coming soon...</Button>
-        </button>
+        <Button class="w-full" href={android_release.apk}>
+          <Download /> APK
+        </Button>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
@@ -108,17 +101,19 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pt-2 pb-2">
-          <i class="animate-pulse fa-brands fa-linux text-9xl"></i>
+          <i class="fa-brands fa-linux text-9xl"></i>
         </div>
-        <!-- <div class="flex flex-grow justify-center"> <Button class="w-1/3 mr-1" href={linux_release.deb}> <Download /> DEB </Button> <Button class="w-1/3 mr-1" href={linux_release.rpm}> <Download /> RPM </Button> <Button class="w-1/3" href={linux_release.appimage}> <Download /> AppImage </Button> </div> -->
-        <button
-          onclick={() =>
-            toast.warning("Not available yet", {
-              description: "this release is still in need for further testing",
-            })}
-        >
-          <Button class="w-full" disabled>Coming soon...</Button>
-        </button>
+        <div class="flex flex-grow justify-center">
+          <Button class="w-1/3 mr-1" href={linux_release.deb}>
+            <Download /> DEB
+          </Button>
+          <Button class="w-1/3 mr-1" href={linux_release.rpm}>
+            <Download /> RPM
+          </Button>
+          <Button class="w-1/3" href={linux_release.appimage}>
+            <Download /> AppImage
+          </Button>
+        </div>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
@@ -134,16 +129,9 @@
       </Card.Header>
       <Card.Content class="grid gap-4">
         <div class="flex justify-center pt-2 pb-2">
-          <i class="animate-pulse fa-brands fa-apple text-9xl"></i>
+          <i class="fa-brands fa-apple text-9xl"></i>
         </div>
-        <button
-          onclick={() =>
-            toast.warning("Not available yet", {
-              description: "this release is still in need for further testing",
-            })}
-        >
-          <Button class="w-full" disabled>Coming soon...</Button>
-        </button>
+        <Button class="w-full" href={mac_release.dmg}><Download /> DMG</Button>
       </Card.Content>
       <Card.Footer>
         <Button class="w-full" variant="secondary" href={github_release}
