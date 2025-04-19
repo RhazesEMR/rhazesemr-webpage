37a2c13dbce4922e5e3b5e6a52a05c01b52cd5cf
Naz <ndpm13@ch-naseem.com>
2025-04-19 16:41:12 +0100

    ✨feat: list some features in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,37 @@
   import { goto } from "$app/navigation";
 
   import Button from "$lib/components/ui/button/button.svelte";
+
+  import * as Card from "$lib/components/ui/card";
+
+  import { Sparkles, ExternalLink } from "lucide-svelte";
+
+  const features = [
+    {
+      title: "Patient Records Management",
+      description:
+        "Effortlessly manage comprehensive patient information in one secure location. Track medical histories, store diagnostic results, and access patient data instantly with our intuitive interface designed for healthcare professionals.",
+    },
+    {
+      title: "Security-First Architecture",
+      description:
+        "Protect sensitive patient data with our security-focused design. Our isolated database containers, encrypted connections, and role-based access controls ensure patient information remains private and secure.",
+    },
+    {
+      title: "Built-in AI Assistant",
+      description:
+        "Enhance clinical workflows with our integrated Saba AI assistant. Powered by Mistral's specialized 24B parameter model, this Arabic-fluent chatbot understands regional nuances and medical terminology to provide culturally relevant support for Middle Eastern healthcare contexts.",
+    },
+  ];
 </script>
 
 <main class="flex flex-grow flex-col items-center">
   <div
-    class="w-full h-full bg-fixed relative bg-cover"
+    class="w-full h-full bg-fixed relative bg-cover bg-center"
     style="background-image: url('./banner.jpg');"
   >
     <div
-      class="flex flex-grow flex-col items-center pt-64 md:pt-32 pb-3 md:pb-8 backdrop-brightness-50 backdrop-blur-sm"
+      class="flex flex-grow flex-col items-center pt-64 md:pt-24 pb-3 md:pb-8 backdrop-brightness-50 backdrop-blur-sm"
     >
       <img class="rounded-md" src="./favicon.png" alt="Logo" width="128" />
       <p class="m-3 text-xl text-white">Rhazes EMR</p>
@@ -38,5 +60,27 @@
         >
       </div>
     </div>
+  </div>
+
+  <div class="m-3 mt-10 text-4xl flex items-center">
+    <Sparkles size={52} />
+    <p class="px-2">Features</p>
+    <Sparkles size={52} />
+  </div>
+
+  <div class="flex flex-wrap justify-center mt-5 max-w-4xl">
+    {#each features as feature}
+      <Card.Root class="m-2">
+        <Card.Content class="flex">
+          <div class="">
+            <h1 class="mb-5 text-3xl">{feature.title}</h1>
+            <p>{feature.description}</p>
+          </div>
+        </Card.Content>
+      </Card.Root>
+    {/each}
+    <Button variant="link" onclick={() => goto("/features")}
+      >See more<ExternalLink />
+    </Button>
   </div>
 </main>
